4c425424fb633c37375483a0eee3eb540a90339f
Deep Manek <dpmanek@gmail.com>
2021-06-06 21:04:17 +0530

    progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
-   <h3>Portfolio</h3>
+   <Navbar/>
   );
 }
 
