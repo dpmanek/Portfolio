9e1959e54f9ea37f9a09ee173366ca0c63cac44b
dpmanek <dpmanek@gmail.com>
2022-12-29 07:00:07 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
 						<Nav.Item className="fork-btn">
 							<Button
-								href="https://github.com/soumyajit4419/Portfolio"
+								href="https://github.com/dpmanek/Portfolio"
 								target="_blank"
 								className="fork-btn-inner"
 							>
