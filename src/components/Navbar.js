fb8ff6a51f8f967d952de7f2df43cacf6c97d28c
Deep Manek <dpmanek@gmail.com>
2024-04-25 07:33:09 -0400

    link fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
 						<Nav.Item className="fork-btn">
 							<Button
-								href="https://github.com/dpmanek/Portfolio"
+								href="https://github.com/dpmanek/"
 								target="_blank"
 								className="fork-btn-inner"
 							>
